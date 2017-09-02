ad8ec6cc41a9367e37e306805f4d254e5feaca06
Edoardo Franco Vianelli <edoardofrancovianelli@Edoardos-MacBook-Pro.local>
2017-09-02 10:30:00 -0700

    First commit

---
+++ Airpact Fire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Airpact Fire
+//
+//  Created by Edoardo Franco Vianelli on 9/2/17.
+//  Copyright © 2017 Edoardo Franco Vianelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
